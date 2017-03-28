bd51dc04e68781ea520ebe80988f18e1f0555000
zuiwuchang <zuiwuchang@gmail.com>
2017-03-28 11:31:17 +0800

    add sdl2/UiButton

---
+++ sdl2/Ui.go
@@ -0,0 +1,55 @@
+package sdl2
+
+const (
+	//鼠標 單擊
+	UI_EVT_LBTN_CLICK = "k-lbtn_click"
+	UI_EVT_RBTN_CLICK = "k-rbtn_click"
+	UI_EVT_MBTN_CLICK = "k-mbtn_click"
+	//鼠標 雙擊
+	UI_EVT_LBTN_DCLICK = "k-lbtn_dclick"
+	UI_EVT_RBTN_DCLICK = "k-rbtn_dclick"
+	UI_EVT_MBTN_DCLICK = "k-mbtn_dclick"
+)
+
+//ui事件 回調
+type UiCallBack func(ui UiObject, info interface{})
+
+type UiObject interface {
+	Object
+	//設置 事件回調
+	SetEventCallback(evt string /*ui事件名*/, callback UiCallBack)
+	//返回 事件回調 函數
+	GetEventCallback(evt string) UiCallBack
+}
+type UiBase struct {
+	Node
+	evtCallbacks map[string]UiCallBack
+}
+
+func posInRect(posX, posY, x, y, w, h int) bool {
+	return posX >= x && posX <= x+w &&
+		posY >= y && posY <= y+h
+}
+func (u *UiBase) posInRect(posX, posY int32) bool {
+	x, y := u.GetPos()
+	x, y = u.ToScreenPos(x, y)
+	w, h := u.GetSize()
+	return posInRect(int(posX), int(posY), int(x), int(y), w, h)
+}
+
+//設置 事件回調
+func (u *UiBase) SetEventCallback(evt string /*ui事件名*/, callback UiCallBack) {
+	if u.evtCallbacks == nil {
+		u.evtCallbacks = make(map[string]UiCallBack)
+	}
+	u.evtCallbacks[evt] = callback
+}
+
+//返回 事件回調 函數
+func (u *UiBase) GetEventCallback(evt string) UiCallBack {
+	if u.evtCallbacks == nil {
+		return nil
+	}
+	callback, _ := u.evtCallbacks[evt]
+	return callback
+}
--- sdl2/UiButton.go
+++ sdl2/UiButton.go
@@ -2,17 +2,20 @@ package sdl2
 
 import (
 	"github.com/veandco/go-sdl2/sdl"
-)
-
-const (
-	UI_BTN_NORMAL = 0
-	UI_BTN_NODOWN = 1
+	"time"
 )
 
 type UiBtuuon struct {
-	Node
+	UiBase
 
 	normalTexture, downTexture *sdl.Texture
+	isLBtnDown                 bool
+	isRBtnDown                 bool
+	isMBtnDown                 bool
+
+	lastLClice time.Time
+	lastRClice time.Time
+	lastMClice time.Time
 }
 
 func NewUiBtuuon(normal, down *sdl.Texture) *UiBtuuon {
@@ -22,4 +25,130 @@ func NewUiBtuuon(normal, down *sdl.Texture) *UiBtuuon {
 	}
 	ui.SetTexture(normal)
 	return ui
+}
+func (u *UiBtuuon) onLBtnEvt(t *sdl.MouseButtonEvent) bool {
+	if t.Type == sdl.MOUSEBUTTONDOWN {
+		if u.posInRect(t.X, t.Y) {
+			u.isLBtnDown = true
+			u.SetTexture(u.downTexture)
+
+			SetFocus(u)
+			return true
+		}
+	} else if t.Type == sdl.MOUSEBUTTONUP {
+		if u.isLBtnDown {
+			u.isLBtnDown = false
+			if !u.isRBtnDown && !u.isMBtnDown {
+				u.SetTexture(u.normalTexture)
+			}
+
+			if u.posInRect(t.X, t.Y) {
+				callback := u.GetEventCallback(UI_EVT_LBTN_CLICK)
+				if callback != nil {
+					callback(u, nil)
+				}
+
+				now := time.Now()
+				if now.Before(u.lastLClice.Add(time.Second)) {
+					u.lastLClice = time.Time{}
+					callback := u.GetEventCallback(UI_EVT_LBTN_DCLICK)
+					if callback != nil {
+						callback(u, nil)
+					}
+				} else {
+					u.lastLClice = now
+				}
+			}
+		}
+	}
+
+	return false
+}
+func (u *UiBtuuon) onRBtnEvt(t *sdl.MouseButtonEvent) bool {
+	if t.Type == sdl.MOUSEBUTTONDOWN {
+		if u.posInRect(t.X, t.Y) {
+			u.isRBtnDown = true
+			u.SetTexture(u.downTexture)
+
+			SetFocus(u)
+			return true
+		}
+	} else if t.Type == sdl.MOUSEBUTTONUP {
+		if u.isRBtnDown {
+			u.isRBtnDown = false
+			if !u.isLBtnDown && !u.isMBtnDown {
+				u.SetTexture(u.normalTexture)
+			}
+			if u.posInRect(t.X, t.Y) {
+				callback := u.GetEventCallback(UI_EVT_RBTN_CLICK)
+				if callback != nil {
+					callback(u, nil)
+				}
+
+				now := time.Now()
+				if now.Before(u.lastRClice.Add(time.Second)) {
+					u.lastRClice = time.Time{}
+					callback := u.GetEventCallback(UI_EVT_RBTN_DCLICK)
+					if callback != nil {
+						callback(u, nil)
+					}
+				} else {
+					u.lastRClice = now
+				}
+			}
+		}
+	}
+
+	return false
+}
+func (u *UiBtuuon) onMBtnEvt(t *sdl.MouseButtonEvent) bool {
+	if t.Type == sdl.MOUSEBUTTONDOWN {
+		if u.posInRect(t.X, t.Y) {
+			u.isMBtnDown = true
+			u.SetTexture(u.downTexture)
+
+			SetFocus(u)
+			return true
+		}
+	} else if t.Type == sdl.MOUSEBUTTONUP {
+		if u.isMBtnDown {
+			u.isMBtnDown = false
+			if !u.isLBtnDown && !u.isRBtnDown {
+				u.SetTexture(u.normalTexture)
+			}
+			if u.posInRect(t.X, t.Y) {
+				callback := u.GetEventCallback(UI_EVT_MBTN_CLICK)
+				if callback != nil {
+					callback(u, nil)
+				}
+
+				now := time.Now()
+				if now.Before(u.lastMClice.Add(time.Second)) {
+					u.lastMClice = time.Time{}
+					callback := u.GetEventCallback(UI_EVT_MBTN_DCLICK)
+					if callback != nil {
+						callback(u, nil)
+					}
+				} else {
+					u.lastMClice = now
+				}
+			}
+		}
+	}
+
+	return false
+}
+func (u *UiBtuuon) OnEvent(evt sdl.Event) bool {
+	switch t := evt.(type) {
+	case *sdl.MouseButtonEvent:
+		switch t.Button {
+		case sdl.BUTTON_LEFT:
+			return u.onLBtnEvt(t)
+		case sdl.BUTTON_RIGHT:
+			return u.onRBtnEvt(t)
+		case sdl.BUTTON_MIDDLE:
+			return u.onMBtnEvt(t)
+		}
+	}
+	return false
 }
