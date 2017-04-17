cf296409a772f4cea07ed6eab581d28c83fcdb01
zuiwuchang <zuiwuchang@gmail.com>
2017-04-17 14:21:17 +0800

    fast algorithm BinarySearch code

--- algorithm/BinarySearch.go
+++ algorithm/BinarySearch.go
@@ -11,19 +11,20 @@ type BinarySearchCompare func(i int) (compare int, e error)
 
 //使用 二分查找 尋找 [low,high] 中 滿足 compare 的 值
 func BinarySearch(low, high int, compareFunc BinarySearchCompare) (int, error) {
-	if low > high {
-		return 0, errors.New("not found")
-	}
-	mid := low + (high-low)/2
+	for low <= high {
+		mid := low + (high-low)/2
+		compare, e := compareFunc(mid)
+		if e != nil {
+			return 0, e
+		}
 
-	compare, e := compareFunc(mid)
-	if e != nil {
-		return 0, e
+		if compare == 0 {
+			return mid, nil
+		} else if compare < 0 {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
 	}
-	if compare == 0 {
-		return mid, nil
-	} else if compare < 0 {
-		return BinarySearch(mid+1, high, compareFunc)
-	}
-	return BinarySearch(low, mid-1, compareFunc)
+	return 0, errors.New("not found")
 }
