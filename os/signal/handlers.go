b18b1d9eef88e3e90fc5f66c18894bf2517a0578
zuiwuchang <zuiwuchang@gmail.com>
2017-08-24 14:02:51 +0800

    add os/signal at win32

---
+++ os/signal/handlers.go
@@ -0,0 +1,36 @@
+package signal
+
+import "C"
+
+import (
+	"os"
+	"sync"
+	"syscall"
+)
+
+const (
+	SIGINT   = 2  /* interrupt */
+	SIGILL   = 4  /* illegal instruction - invalid function image */
+	SIGFPE   = 8  /* floating point exception */
+	SIGSEGV  = 11 /* segment violation */
+	SIGTERM  = 15 /* Software termination signal from kill */
+	SIGBREAK = 21 /* Ctrl-Break sequence */
+	SIGABRT  = 22
+)
+
+type handlers struct {
+	sync.Mutex
+	m map[chan<- os.Signal]int
+}
+
+var g_handlers handlers
+
+//export _king_go_os_signal_goHandler
+func _king_go_os_signal_goHandler(sig int) {
+	g_handlers.Lock()
+	defer g_handlers.Unlock()
+
+	for ch, _ := range g_handlers.m {
+		ch <- syscall.Signal(sig)
+	}
+}
---
+++ os/signal/singal.go
@@ -0,0 +1,61 @@
+//go 標準庫的 os.signal 無法接收 win32 的 raise 產生的 signal 此庫完成此功能
+package signal
+
+//#cgo LDFLAGS:
+//#include <signal.h>
+//extern void _king_go_os_signal_goHandler(int);
+/*
+void _king_go_os_signal_handler(int sig)
+{
+	_king_go_os_signal_goHandler(sig);
+	signal(sig,_king_go_os_signal_handler);
+}
+void _king_go_os_signal_signals()
+{
+	//註冊 信號處理函數
+	signal(SIGINT,_king_go_os_signal_handler);		//通常是 ctrl + c
+	signal(SIGILL,_king_go_os_signal_handler);
+	signal(SIGFPE,_king_go_os_signal_handler);
+	signal(SIGSEGV,_king_go_os_signal_handler);
+	signal(SIGTERM,_king_go_os_signal_handler);	//軟體退出 kill
+	signal(SIGBREAK,_king_go_os_signal_handler);	//通常是 ctrl + break/pause /點擊了控制檯窗口的 關閉按鈕
+	signal(SIGABRT,_king_go_os_signal_handler);	//調用了 abort 函數
+}
+*/
+import "C"
+
+import (
+	"os"
+	osSignal "os/signal"
+	"runtime"
+)
+
+func doInit() {
+	if g_handlers.m != nil {
+		return
+	}
+
+	g_handlers.m = make(map[chan<- os.Signal]int)
+
+	C._king_go_os_signal_signals()
+}
+
+func Notify(c chan<- os.Signal, sig ...os.Signal) {
+	if runtime.GOOS == "windows" {
+		g_handlers.Lock()
+		doInit()
+		g_handlers.m[c] = 1
+		g_handlers.Unlock()
+	} else {
+		osSignal.Notify(c, sig...)
+	}
+}
+func Stop(c chan<- os.Signal) {
+	if runtime.GOOS == "windows" {
+		g_handlers.Lock()
+		delete(g_handlers.m, c)
+		g_handlers.Unlock()
+	} else {
+		osSignal.Stop(c)
+	}
+}
