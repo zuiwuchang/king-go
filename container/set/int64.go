1fcb4e0e0e046cfc96ffe27cb63c8a3d7daca09f
zuiwuchang <zuiwuchang@gmail.com>
2017-11-30 09:50:07 +0800

    rename setint64 to int64

--- container/set/int64.go
+++ container/set/int64.go
@@ -7,34 +7,34 @@ import (
 
 var StringSetEmpty string = "[]"
 
-type SetInt64 map[int64]bool
+type Int64 map[int64]bool
 
 //創建 一個 Int64 的 set
-func NewInt64() SetInt64 {
-	return make(SetInt64)
+func NewInt64() Int64 {
+	return make(Int64)
 }
 
 //插入節點
-func (this SetInt64) Insert(key int64) {
+func (this Int64) Insert(key int64) {
 	this[key] = true
 }
 
 //移除節點
-func (this SetInt64) Remove(key int64) {
+func (this Int64) Remove(key int64) {
 	delete(this, key)
 }
 
 //返回節點是否 存在
-func (this SetInt64) Ok(key int64) bool {
+func (this Int64) Ok(key int64) bool {
 	_, ok := this[key]
 	return ok
 }
 
 //返回節點 數量
-func (this SetInt64) Len() int {
+func (this Int64) Len() int {
 	return len(this)
 }
-func (this SetInt64) String() string {
+func (this Int64) String() string {
 	if len(this) == 0 {
 		return StringSetEmpty
 	} else if len(this) == 1 {
